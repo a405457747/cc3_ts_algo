512535f0685633267e975899275f1304c34f6882
Sky.Allen <894982165@qq.com>
2024-05-01 08:00:27 +0800

    3

--- assets/leetcode/MainGo.ts
+++ assets/leetcode/MainGo.ts
@@ -5,19 +5,19 @@ const { ccclass, property } = _decorator;
 @ccclass('Main')
 export class MainGo extends Component {
 
-    
-   add(arr:number[]):number {
-    let res=0;
-    for(let item of arr){
-        res+=item;
-    }
-    return res;
-   }
+
 
     protected start(): void {
 
-        let k=[3,4,5];
-        console.log(this.add(k))
+        /**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+
+};
+
+
     }
 }
 
